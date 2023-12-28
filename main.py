ff54804c2108f15d95cbf8bf4454a6ac51f4e5cb
Ivan <89919994+IvanMarie1@users.noreply.github.com>
2023-12-28 20:04:28 +0100

    Update main.py
    Pas mal de modifications mais en gros:
    
    - Fin de la méthode carte_valide avec des changements de logique pour que ce soit plus simple
    - Fin de la méthode tour avec l'ajout des joueurs d'après
    - Ajout de la méthode jouer dans la classe Joueur (pose 1 carte)
    - Ajout des attributs pos et cartes_gagnees dans la classe Joueur
    - Mise à jour de la méthode maitre dans Pile pour que ça prenne en compte les atouts (à vérifier quand même)

--- main.py
+++ main.py
@@ -3,23 +3,25 @@ import random as r
 
 
 class Carte:
-    """Classe pour représenter une carte de jeu
+    """Classe représentant une carte de jeu
 
     ---
     ### Attributs :
-    - points: dict \n
-            points d'une carte en fonction de sa valeur
-    - points_atout: dict \n
-            points d'une carte en fonction de sa valeur en étant atout
-    - valeur: str \n
-            valeur de la carte (ex: J = Valet)
-    - couleur: str \n
-            couleur de la carte (ex ♦ = Carreau)
-
+    points: dict
+        points d'une carte en fonction de sa valeur
+    
+    points_atout: dict
+        points d'une carte en fonction de sa valeur en étant atout
+    
+    valeur: str
+        valeur de la carte (ex: J = Valet)
+    
+    couleur: str
+        couleur de la carte (ex ♦ = Carreau)
     ---
     ### Méthode
-    - point()\n
-            Renvoie les points de la carte
+    point()
+        Renvoie les points de la carte
     """
     
     points: dict = {'A': 11, 'K': 4, 'Q': 3, 'J': 2, '10': 10, '9': 0, '8': 0, '7': 0}
@@ -27,10 +29,11 @@ class Carte:
 
     def __init__(self, valeur: str, couleur: str) -> None:
         """### Paramètres :
-        - valeur: str \n
-                valeur de la carte (ex: J = Valet)
-        - couleur: str \n
-                couleur de la carte (ex ♦ = Carreau)
+        valeur: str
+            valeur de la carte (ex: J = Valet)
+        
+        couleur: str
+            couleur de la carte (ex ♦ = Carreau)
         """
         self.valeur: str = valeur
         self.couleur: str = couleur
@@ -44,8 +47,9 @@ class Carte:
         
         ---
         ### Paramètres
-        - atout: str (optionnel) \n
-                Couleur de l'atout en jeu"""
+        atout: str (optionnel)
+            Couleur de l'atout en jeu
+        """
         if self.couleur == atout:
             return self.points_atout[self.valeur]
         else:
@@ -56,23 +60,27 @@ class Carte:
 
 
 class Pile:
-    """Une classe pour représenter une pile de cartes
+    """Classe représentant une pile de cartes
 
     ---
     ### Attributs :
-    - cartes : list[Carte] \n
-            une liste d'objets Carte pour stocker les cartes qui sont dans la pile
+    
+    cartes : list[Carte]
+        une liste d'objets Carte pour stocker les cartes qui sont dans la pile
 
     --- 
     ### Méthodes :
-    - couper()\n
-            modifier cartes comme quand on coupe la pile
-    - distribuer() \n 
-            ajoute des cartes à un joueur et les enlève de la pile
-    - couleurs() \n
-            renvoie la liste des couleurs de chaque carte
-    - maitre() \n
-            renvoie la valeur de la carte maitresse ainsi que sa position dans la pile
+    couper()
+        modifier cartes comme quand on coupe la pile
+    
+    distribuer() 
+        ajoute des cartes à un joueur et les enlève de la pile
+    
+    couleurs()
+        renvoie la liste des couleurs de chaque carte
+    
+    maitre()
+        renvoie la valeur de la carte maitresse ainsi que sa position dans la pile
     """
 
 
@@ -88,17 +96,19 @@ class Pile:
         milieu = len(self.cartes) // 2
         self.cartes = self.cartes[milieu:] + self.cartes[:milieu]
     
-    def distribuer(self, nb:int, joueur:object) -> None:
+    def distribuer(self, nb:int, tas:list) -> None:
         """Ajoute des cartes à un joueur et les retire de la pile
         
         ---
         ### Paramètres
-        - nb: int \n
-                Nombre de cartes à distribuer
-        - joueur: object \n
-                Joueur qui reçoit les cartes"""
+        nb: int
+            Nombre de cartes à distribuer
+
+        tas: list 
+            Tas du joueur qui reçoit les cartes
+            """
         
-        joueur.cartes += self.cartes[:nb]
+        tas += self.cartes[:nb]
         self.cartes = self.cartes[nb:]
     
     def couleurs(self) -> list[str]:
@@ -113,15 +123,27 @@ class Pile:
         
         ---
         ### Paramètres
-        - atout: str (optionnel) \n
-                Couleur de l'atout dans le jeu
+        atout: str (optionnel)
+            Couleur de l'atout dans le jeu
         """
         i_max = 0
         pt_max = 0
+        atout_joue = False
         for i in range(len(self.cartes)):
-            if self.cartes[i].point(atout) > pt_max:
+            carte = self.cartes[i]
+            pt = carte.point(atout)
+
+            if (not atout_joue) and carte.couleur == atout:
+                atout_joue = True
                 i_max = i
-                pt_max = self.cartes[i].point(atout)
+                pt_max = pt
+            elif carte.couleur == atout and pt > pt_max and atout_joue:
+                i_max = i
+                pt_max = pt
+            elif pt > pt_max and (not atout_joue):
+                i_max = i
+                pt_max = pt
+
         return (pt_max, i_max)
 
 
@@ -132,41 +154,82 @@ class Joueur:
     
     ---
     ### Attributs
-    - cartes: list[Carte] \n
-            cartes que le joueur a en main
-    - nom: str \n
-            nom du joueur
-
+    cartes: list[Carte]
+        cartes que le joueur a en main
+    
+    cartes_gagnees: list[Carte]
+        cartes que le joueur a gagné
+    
+    nom: str
+        nom du joueur
+    
+    pos: int
+        position du joueur dans le jeu
     ---
     ### Méthodes
-    - distribuer() \n
-            Le joueur donne des cartes à une pile
+    distribuer()
+        Le joueur donne des cartes à une pile
+    
+    jouer()
+        Le jouer pose une carte dans une pile
+    
+    couleurs()
+        Renvoie les couleurs des cartes du joueur
     """
     
     
-    def __init__(self, nom: str) -> None:
+    def __init__(self, nom: str, pos: int) -> None:
         """### Paramètres
-        - nom: str \n
-                nom du joueur"""
+        nom: str
+            nom du joueur
+
+        pos: int
+            position du joueur dans le jeu
+        """
         self.nom = nom
+        self.pos = pos
         self.cartes:list[Carte] = []
+        self.cartes_gagnees: list[Carte] = []
     
     def __repr__(self) -> str:
         """Affiche la main du joueur"""
         return " | ".join(map(repr, self.cartes))
 
-    def distribuer(self, nb:int, pile:object) -> None:
+    def distribuer(self, nb:int, pile:Pile) -> None:
         """Ajoute des cartes à une pile et les retire du jeu du joueur
         
         ---
         ### Paramètres
-        - nb: int \n
-                Nombre de cartes à distribuer
-        - pile: object \n
-                Pile qui reçoit les cartes"""
+        nb: int 
+            Nombre de cartes à distribuer
+
+        pile: Pile
+            Pile qui reçoit les cartes"""
         
         pile.cartes += self.cartes[:nb]
         self.cartes = self.cartes[nb:]
+    
+    def couleurs(self) -> list[str]:
+        """Renvoie la liste des couleurs de chaque carte du joueur"""
+        result = []
+        for carte in self.cartes:
+            result.append(carte.couleur)
+        return result
+
+    def jouer(self, i_carte: int, pile: Pile) -> None:
+        """Joue une carte et la pose dans une pile
+        
+        ---
+        ### Paramètres
+        i_carte: int
+            position de la carte dans le jeu du joueur
+
+        pile: Pile 
+            pile qui reçoit la carte
+        """
+        pile.cartes.append(self.cartes[i_carte - 1]) 
+        self.cartes = self.cartes[:i_carte - 1] + self.cartes[i_carte:]
+
 
 
 
@@ -176,39 +239,46 @@ class Jeu:
     
     ---
     ### Attributs
-    - atout: str \n
-            Couleur de l'atout à un moment du jeu
-    - i_donneur: int \n
-            Position du donneur dans la liste de joeurs
-    - joueur: list[Joueur] \n
-            Liste des joueurs dans la partie
-    - pile: Pile \n
-            Pile des cartes de la partie
-    - pli: Pile \n
-            Pile des cartes qui sont en jeu dans un tour
+    atout: str
+        Couleur de l'atout à un moment du jeu
+    
+    i_donneur: int
+        Position du donneur dans la liste de joeurs
+    
+    joueur: list[Joueur]
+        Liste des joueurs dans la partie
+    
+    pile: Pile
+        Pile des cartes de la partie
+    
+    pli: Pile
+        Pile des cartes qui sont en jeu dans un tour
         
     ---
     ## Méthodes
-    - tour_atout() \n
-            Lance un tour où on choisit la couleur de l'atout
-    - distribution() \n
-            Distribue les cartes aux joueurs et propose l'atout
-    - tour() \n
-            Lance un tour du jeu où les joueurs posent leur carte
-    - carte_valide() \n
-            Test la validité de la carte que l'on veut jouer
+    tour_atout()
+        Lance un tour où on choisit la couleur de l'atout
+    
+    distribution()
+        Distribue les cartes aux joueurs et propose l'atout
+    
+    tour()
+        Lance un tour du jeu où les joueurs posent leur carte
+    
+    carte_valide()
+        Test la validité de la carte que l'on veut jouer
     """
     
+    def __init__(self) -> None:
+        self.atout:str = ""
+        self.i_donneur = r.randint(0,3)
+        self.joueurs:list[Joueur] = [Joueur(nom, i) for i, nom in enumerate('J1 J2 J3 J4'.split(), 0)]
 
-    atout:str = ""
-    i_donneur = r.randint(0,3)
-    joueurs:list[Joueur] = [Joueur(nom) for nom in 'J1 J2 J3 J4'.split()]
+        self.pile:Pile = Pile()
+        self.pile.cartes = [Carte(val, coul) for val in 'A K Q J 10 9 8 7'.split() for coul in '♥ ♦ ♣ ♠'.split()]
+        r.shuffle(self.pile.cartes)
 
-    pile:Pile = Pile()
-    pile.cartes = [Carte(val, coul) for val in 'A K Q J 10 9 8 7'.split() for coul in '♥ ♦ ♣ ♠'.split()]
-    r.shuffle(pile.cartes)
-
-    pli: Pile = Pile()
+        self.pli: Pile = Pile()
 
 
     def tour_atout(self) -> None:
@@ -222,7 +292,7 @@ class Jeu:
             print(f"{joueur.nom} : {joueur}")
             rep = input(f"{joueur.nom}, veux-tu l'atout ? (o/n) ")
             if rep == "o":
-                self.pile.distribuer(1, joueur)
+                self.pile.distribuer(1, joueur.cartes)
                 self.atout = carte_atout.couleur
                 return
             
@@ -248,10 +318,10 @@ class Jeu:
             self.i_donneur += 1
             for i in range(4):
                 joueur = self.joueurs[(self.i_donneur + i + 1)%4]
-                self.pile.distribuer(3, joueur)
+                self.pile.distribuer(3, joueur.cartes)
             for i in range(4):
                 joueur = self.joueurs[(self.i_donneur + i + 1)%4]
-                self.pile.distribuer(2, joueur)
+                self.pile.distribuer(2, joueur.cartes)
             
             self.tour_atout()
 
@@ -259,60 +329,100 @@ class Jeu:
         for i in range(4):
             joueur = self.joueurs[(self.i_donneur + i + 1)%4]
             if len(joueur.cartes) == 5:
-                self.pile.distribuer(3, joueur)
+                self.pile.distribuer(3, joueur.cartes)
             else:
-                self.pile.distribuer(2, joueur)
+                self.pile.distribuer(2, joueur.cartes)
 
 
     def tour(self) -> None:
         """Lance un tour du jeu et le joueur avec la meilleur carte remporte le pli et joue au tour suivant"""
-        print(f"{self.joueurs[(self.i_donneur + 1)%4]}")
+        
         for i in range(4):
             joueur = self.joueurs[(self.i_donneur + 1 + i)%4]
 
             # premier joueur
-            if i == 0:
-                input(f"Au tour de {joueur.nom}")
-                print(f"{joueur.nom}: {joueur}")
-                i_carte = int(input(f"Quelle carte voulez-vous jouer (1-{len(joueur.cartes)})"))
+            
+            input("\n" * 5 + f"Au tour de {joueur.nom}")
+            print(f"{joueur.nom}: {joueur}")
+
+            print(f"Carte posées : {self.pli}")
+
+            i_carte = int(input(f"Quelle carte voulez-vous jouer ? (1-{len(joueur.cartes)})"))
+
+            if i == 0: # premier joueur (pas de restriction)
                 while 1 > i_carte  or i_carte > len(joueur.cartes):
                     print("Carte non valide")
                     i_carte = int(input(f"Quelle carte voulez-vous jouer (1-{len(joueur.cartes)})"))
 
-                joueur.distribuer(1, self.pli)
-
-            # autres joueurs
-            else:
-                input(f"Au tour de {joueur.nom}")
-                print(f"{joueur.nom}: {joueur}")
-                i_carte = int(input(f"Quelle carte voulez-vous jouer (1-{len(joueur.cartes)})"))
-                while 1 > i_carte  or i_carte > len(joueur.cartes):
+            else: # autres joueurs
+                while not self.carte_valide(i_carte, joueur):
                     print("Carte non valide")
                     i_carte = int(input(f"Quelle carte voulez-vous jouer (1-{len(joueur.cartes)})"))
+
+            joueur.jouer(i_carte, self.pli)
+
+        i_maitre = (self.pli.maitre(self.atout)[1] + self.i_donneur + 1) % 4
+        self.pli.distribuer(4, self.joueurs[i_maitre].cartes_gagnees)
+        self.i_donneur = i_maitre - 1
+
         
     def carte_valide(self, i_carte: int, joueur: Joueur) -> bool:
-        """Renvoie True si la carte peut être posée et False sinon"""
+        """Renvoie True si la carte peut être posée et False sinon
+        
+        ---
+        ### Paramètres 
+        i_carte: int 
+            Position de la carte dans le jeu du joueur
+
+        joueur: Joueur
+            Joueur qui pose la carte
+        """
         if 1 > i_carte  or i_carte > len(joueur.cartes):
             return False
         
         carte = joueur.cartes[i_carte - 1]
+        pt_maitre, i_maitre = self.pli.maitre(self.atout)
+        i_maitre = (i_maitre + self.i_donneur + 1) % 4
 
-        if self.atout in self.pli.couleurs(): # un atout a été posé avant
-            if carte.couleur == self.atout: # le joueur joue un atout
-                if carte.point(self.atout) >= self.pli.maitre(self.atout)[0]: # le joueur monte à l'atout
-                    return True
-                # meilleur atout dans le jeu du joueur ? True ; False
-            # le joueur n'a pas d'atout dans le jeu ? True ; False
-        # carte jouée même couleur : True
-        # carte même couleur dans le jeu : False
-        # equipier maitre : True
-        # carte jouée est atout : True
-        # Le joueur n'a pas d'atout dans son jeu : True False
+        
+        if self.atout in self.pli.couleurs(): # un atout a été posé avant 
+            
+            if not self.atout in joueur.couleurs(): # le joueur n'a pas d'atout
+                return True 
+            if carte.couleur != self.atout: # le joueur ne joue pas atout alors qu'il peut
+                return False
+            if carte.point(self.atout) >= pt_maitre: # le joueur monte à l'atout
+                return True
+            
+            for carte_temp in joueur.cartes:
+                if carte_temp.point(self.atout) >= pt_maitre: # le joueur peut monter à l'atout
+                    return False
+            return True
+                
+        
+        if carte.couleur == self.pli.cartes[0].couleur: # on joue la même couleur que l'entame
+            return True
+        if self.pli.cartes[0].couleur in joueur.couleurs(): # on peut jouer de la même couleur que l'entame
+            return False
+        if (i_maitre - joueur.pos) % 2 == 0: # l'equipier est maitre (ecart entre les joueurs = 2)
+            return True
+        if carte.couleur == self.atout: # on joue atout
+            return True
+        return not self.atout in joueur.couleurs() # on ne peut pas jouer atout
                 
 
                 
     # TODO
     # Mettre une manche : 8 plis + compter les points
-    # Pli : chaque joueur à partir du donneur pose une carte (règles de pose) et à la fin le meilleur remporte le pli
     # Comptage : On compte les points par équipe avec toutes les règles (voir site) et on l'ajoute à un compteur de la partie
     # On repète les manches tant que le score max < 1001
+
+
+# debug
+j = Jeu()
+j.distribution()
+for i in range(8):
+    j.tour()
+
+for joueur in j.joueurs:
+    print(joueur.cartes_gagnees)
